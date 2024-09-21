a6d40433a84f3d0f111c61d636a33e0203dc3e18
tharuAnish <namoanishtharu@gmail.com>
2024-09-21 07:06:12 +0530

    pagination button created

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,5 +1,14 @@
-import React from "react"
+import { Button } from "../ui/button"
 
 export default function Pagination() {
-  return <div>Pagination</div>
+  return (
+    <div className="flex justify-between mt-6 space-x-4">
+      <Button variant="destructive" className="w-24">
+        Previous
+      </Button>
+      <Button variant="destructive" className="w-24">
+        Next
+      </Button>
+    </div>
+  )
 }
