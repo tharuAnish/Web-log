5bb0289ac058c3d317e0cf1d6eea7df66994fcf3
tharuAnish <namoanishtharu@gmail.com>
2024-09-22 19:34:37 +0530

    created actions for categories and replaced with mock caregories

--- src/action/index.js
+++ src/action/index.js
@@ -1,6 +1,7 @@
 "use server"
 
 import { signIn, signOut } from "@/utils/auth"
+import prisma from "@/utils/connect"
 
 // Auth
 export async function loginWithGithub() {
@@ -13,4 +14,15 @@ export async function loginWithGoogle() {
 
 export async function logoutAction() {
   await signOut()
+}
+
+// action to fetch categories
+export const fetchCategories = async () => {
+  try {
+    const categories = await prisma.category.findMany()
+    return categories
+  } catch (err) {
+    console.error("Error fetching categories:", err)
+    throw new Error("Failed to fetch categories")
+  }
 }
--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,120 +1,29 @@
 import Link from "next/link"
 import Image from "next/image"
-
-// const getData = async () => {
-//   const res = await fetch("http://localhost:3000/api/categories", {
-//     cache: "no-store",
-//   });
-
-//   if (!res.ok) {
-//     throw new Error("Failed");
-//   }
-
-//   return res.json();
-// };
+import { fetchCategories } from "@/action"
 
 const CategoryList = async () => {
-  // const data = await getData()
+  const categories = await fetchCategories()
   return (
     <div className="">
       <h1 className="mt-9 mb-8 text-3xl font-bold">Popular Categories</h1>
       <div className=" grid  mx-auto sm:w-auto grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-6 gap-3">
-        {/* {data?.map((item) => (
+        {categories?.map((category) => (
           <Link
+            key={category.id}
             href="/blog?cat=style"
-            className={`flex items-center gap-2 capitalize w-1/6 h-20 justify-center rounded-lg ${styles[item.slug]}`}
-            key={item._id}
+            className={`flex items-center flex-1 gap-5 py-3 pl-4 pr-7  bg-slate-200 capitalize   justify-center rounded-lg `}
           >
-            {item.img && (
-              <Image
-                src={item.img}
-                alt=""
-                width={32}
-                height={32}
-                className="rounded-full"
-              />
-            )}
-            {item.title}
+            <Image
+              src="/fashion.png"
+              alt=""
+              width={32}
+              height={32}
+              className="rounded-full h-9 w-9"
+            />
+            {category.title}
           </Link>
-        ))} */}
-        <Link
-          href="/blog?cat=style"
-          className={`flex items-center flex-1 gap-5 py-3 pl-4 pr-7  bg-slate-200 capitalize   justify-center rounded-lg `}
-        >
-          <Image
-            src="/fashion.png"
-            alt=""
-            width={32}
-            height={32}
-            className="rounded-full h-9 w-9"
-          />
-          fashion
-        </Link>
-        <Link
-          href="/blog?cat=style"
-          className={`flex items-center flex-1 gap-5 py-3 pl-4 pr-7  bg-slate-200 capitalize   justify-center rounded-lg `}
-        >
-          <Image
-            src="/food.png"
-            alt=""
-            width={32}
-            height={32}
-            className="rounded-full h-9 w-9"
-          />
-          food
-        </Link>
-        <Link
-          href="/blog?cat=style"
-          className={`flex items-center flex-1 gap-5 py-3 pl-4 pr-7  bg-slate-200 capitalize   justify-center rounded-lg `}
-        >
-          <Image
-            src="/culture.png"
-            alt=""
-            width={32}
-            height={32}
-            className="rounded-full h-9 w-9"
-          />
-          culture
-        </Link>
-        <Link
-          href="/blog?cat=style"
-          className={`flex items-center flex-1 gap-5 py-3 pl-4 pr-7  bg-slate-200 capitalize   justify-center rounded-lg `}
-        >
-          <Image
-            src="/fashion.png"
-            alt=""
-            width={32}
-            height={32}
-            className="rounded-full h-9 w-9"
-          />
-          fashion
-        </Link>
-        <Link
-          href="/blog?cat=style"
-          className={`flex items-center flex-1 gap-5 py-3 pl-4 pr-7  bg-slate-200 capitalize   justify-center rounded-lg `}
-        >
-          <Image
-            src="/travel.png"
-            alt=""
-            width={32}
-            height={32}
-            className="rounded-full h-9 w-9"
-          />
-          travel
-        </Link>
-        <Link
-          href="/blog?cat=style"
-          className={`flex items-center flex-1 gap-5 py-3 pl-4 pr-7  bg-slate-200 capitalize   justify-center rounded-lg `}
-        >
-          <Image
-            src="/culture.png"
-            alt=""
-            width={32}
-            height={32}
-            className="rounded-full h-9 w-9"
-          />
-          culture
-        </Link>
+        ))}
       </div>
     </div>
   )
