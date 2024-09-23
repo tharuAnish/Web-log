c49cf48a8eaadab2e2ffa3c946b461272bae6a5b
tharuAnish <namoanishtharu@gmail.com>
2024-09-23 08:29:05 +0530

    post schema and get post done

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -3,15 +3,26 @@ import Pagination from "../pagination/Pagination"
 import Image from "next/image"
 import Card from "../card/Card"
 
-export default function CardList() {
+const getData = async (page) => {
+  const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
+    cache: "no-store",
+  })
+
+  if (!res.ok) {
+    throw new Error("Failed")
+  }
+
+  return res.json()
+}
+export default async function CardList({ page }) {
+  const posts = await getData(page)
   return (
     <div className="grid-cols-5 ">
       <h1 className="text-3xl font-bold my-12">Recent Posts</h1>
       <div className="space-y-9">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {posts?.map((post) => (
+          <Card post={post} />
+        ))}
       </div>
       <Pagination />
     </div>
