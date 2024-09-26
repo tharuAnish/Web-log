28ab62a095c760d6f3d81a2367e97211594aee78
tharuAnish <namoanishtharu@gmail.com>
2024-09-26 08:54:56 +0530

    changed from image to icon in category

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,27 +1,41 @@
 import Link from "next/link"
-import Image from "next/image"
+
 import { fetchCategories } from "@/action"
+import {
+  FaLaptopCode,
+  FaPlane,
+  FaAppleAlt,
+  FaHeartbeat,
+  FaMoneyBillWave,
+  FaUserFriends,
+} from "react-icons/fa"
+
+// Map of categories to their respective icons
+const categoryIcons = {
+  health: <FaHeartbeat />,
+  lifestyle: <FaUserFriends />,
+  technology: <FaLaptopCode />,
+  travel: <FaPlane />,
+  food: <FaAppleAlt />,
+  finance: <FaMoneyBillWave />,
+}
 
 const CategoryList = async () => {
   const categories = await fetchCategories()
   return (
-    <div className="">
-      <h1 className="mt-9 mb-8 text-3xl font-bold">Popular Categories</h1>
-      <div className=" grid  mx-auto sm:w-auto grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-6 gap-3">
+    <div>
+      <h1 className="mt-9 mb-7 text-3xl font-bold">Popular Categories</h1>
+      <div className="grid mx-auto sm:w-auto grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4">
         {categories?.map((category) => (
           <Link
             key={category.id}
             href={`/blog?cat=${category.slug}`}
-            className={`flex items-center flex-1 gap-5 py-3 pl-4 pr-7  bg-slate-200 capitalize   justify-center rounded-lg `}
+            className={`flex items-center flex-1 gap-3 py-3 pl-4 pr-7 border capitalize rounded-lg transition duration-200 hover:bg-secondary`}
           >
-            <Image
-              src="/fashion.png"
-              alt=""
-              width={32}
-              height={32}
-              className="rounded-full h-9 w-9"
-            />
-            {category.title}
+            <span className="h-6 w-6 flex-shrink-0 flex items-center justify-center">
+              {categoryIcons[category.slug]}
+            </span>
+            <span className=" font-medium">{category.title}</span>
           </Link>
         ))}
       </div>
