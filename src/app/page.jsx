dce9d332070da9d779ae8fd46051c8a5bd6b67c0
tharuAnish <namoanishtharu@gmail.com>
2024-09-29 16:28:24 +0530

    category select in blogs page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,6 @@ import Featured from "@/components/featured/Featured"
 import CategoryList from "@/components/categoryList/CategoryList"
 import CardList from "@/components/cardList/CardList"
 import Menu from "@/components/menu/Menu"
-import { Separator } from "@/components/ui/separator"
 
 export default function Home({ searchParams }) {
   const page = parseInt(searchParams.page) || 1
@@ -13,6 +12,7 @@ export default function Home({ searchParams }) {
       <CategoryList />
       <div className="grid grid-cols-12 gap-8">
         <div className="col-span-8 ">
+          <h1 className="text-3xl font-bold mt-14 mb-7">Recent Posts</h1>
           <CardList page={page} />
         </div>
         <div className="col-span-4 pl-8  border-l mt-28">
